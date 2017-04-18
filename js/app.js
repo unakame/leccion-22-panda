55a0982f819f80120d621aab3a920facdd56f4a8
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-18 11:52:22 -0500

    cuadrando2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-  document.getElementById("origen").addEventListener("click", info);
+document.getElementById("origen").addEventListener("click", info);
 
   function info(){
-    document.getElementById("texto1").innerHTML="Lorem ipsum"
+    document.getElementById("texto1").innerHTML="<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur minus consectetur tempora exercitationem, odit eos perspiciatis animi, doloribus dolores quis itaque repudiandae aperiam numquam quos obcaecati veritatis ratione officiis soluta.</p>  <br><br>  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex perspiciatis autem debitis quo saepe dolores asperiores aspernatur explicabo magni unde suscipit optio repellat, esse alias non deserunt quis facere obcaecati?</p>"
+  }
+
+document.getElementById("historia").addEventListener("click",histo);
+
+  function histo(){
+    document.getElementById("texto2").innerHTML="<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit amet corrupti necessitatibus eaque culpa adipisci voluptatibus veritatis, maxime laboriosam, soluta odit deserunt commodi laudantium accusamus quia. Commodi itaque laudantium nobis.</p>  <br><br><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sunt voluptates minus praesentium amet vitae libero porro error quam animi! Laboriosam harum ipsam porro, sapiente perspiciatis expedita nostrum et doloremque.</p>"
   }
